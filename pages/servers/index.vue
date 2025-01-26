67b947929c3f11b02bf7aab6225be2c1a650d760
Anthony Destenay <anthony.destenay@gmail.com>
2025-01-26 17:08:50 +0100

    Added servers filter

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -5,11 +5,83 @@ const _difficulties = {
     normal: 'normal',
     hard: 'difficile',
 };
+
+const scenarioFilter = ref('');
+const difficultyFilter = ref('');
+const onlyOpen = ref(true);
+
+const filteredServers = computed(() => {
+
+    return data.value.servers
+        .filter((server) => {
+            return (onlyOpen.value === false) ? true : server.closed === false;
+        })
+        .filter((server) => {
+            if(scenarioFilter.value) {
+                return server.scenario.id === scenarioFilter.value;
+            }
+            return true;
+        })
+        .filter((server) => {
+            if (difficultyFilter.value) {
+                return server.difficulty === difficultyFilter.value;
+            }
+            return true;
+        });
+});
+
+function compareNames(a, b) {
+    var nameA = a.name.toLowerCase();
+    var nameB = b.name.toLowerCase();
+
+    if(nameA < nameB) {
+        return -1;
+    }
+    if(nameA > nameB) {
+        return 1;
+    }
+
+    return 0;
+}
+
+const listScenarios = computed(() => {
+    return data.value.servers?.map(server => { 
+        return {id: server.scenario.id, name: server.scenario.name}
+    }).reduce((acc, obj) => {
+        if (!acc.some(item => item.id === obj.id)) {
+            acc.push(obj);
+        }
+        return acc;
+    }, []).sort(compareNames);
+    
+});
+
+const difficulties = {
+    'easy': 'Débutant',
+    'normal': 'Normal',
+    'hard': 'Difficile'
+}
 </script>
 
 <template>
     <div class="container mx-auto my-6">
-        <h1>Serveurs</h1>
+        <div class="flex flex-col md:flex-row gap-4 justify-between">
+            <h1>Serveurs</h1>
+            <div class="flex flex-col sm:flex-row gap-2 items-start sm:items-center" v-if="data?.servers">
+                <select v-model="scenarioFilter" class="select select-bordered select-sm">
+                    <option value="">-- Scénario --</option>
+                    <option v-for="scenario in listScenarios" :key="scenario.id" :value="scenario.id">{{ scenario.name }}</option>
+                </select>
+                <select v-model="difficultyFilter" class="select select-bordered select-sm">
+                    <option value="">-- Difficulté --</option>
+                    <option v-for="difficulty, key in difficulties" :key="key" :value="key">{{ difficulty }}</option>
+                </select>
+                <label class="flex gap-2 items-center">
+                    <input type="checkbox" class="toggle toggle-sm toggle-accent" v-model="onlyOpen">
+                    Ouverts uniquement
+                </label>
+            </div>
+        </div>
         <AppLoading v-if="status === 'pending'" />
         <div
             class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4 my-6"
@@ -17,7 +89,7 @@ const _difficulties = {
         >
             <div
                 class="bg-base-200/75 hover:bg-base-300 p-4 text-center rounded-box"
-                v-for="server in data.servers"
+                v-for="server in filteredServers"
                 :key="server.id"
             >
                 <div class="text-xs">
